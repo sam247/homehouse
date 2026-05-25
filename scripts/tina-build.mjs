1d0aa201617ac0b0547bc7e97f43a2afb6eda4a9
sam247 <sampettiford@googlemail.com>
2026-05-25 19:34:57 +0100

    Fix Tina build on Vercel

--- scripts/tina-build.mjs
+++ scripts/tina-build.mjs
@@ -5,11 +5,13 @@ const cloud =
 
 const args = ["build", "--noTelemetry"];
 
-if (!cloud) {
+if (cloud) {
+  args.push("--content=local");
+  args.push("--skip-cloud-checks", "--skip-indexing");
+} else {
   args.push("--local", "--skip-indexing", "--skip-cloud-checks");
 }
 
 const res = spawnSync("tinacms", args, { stdio: "inherit" });
 
 process.exit(typeof res.status === "number" ? res.status : 1);
-
