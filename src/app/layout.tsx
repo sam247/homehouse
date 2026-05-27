f9f29006d0ec372a5c138de8c615362f927b0235
sam247 <sampettiford@googlemail.com>
2026-05-27 11:57:01 +0100

    Add GA4 gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,6 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9X8N3NVR1B" />
+        <Script id="ga4">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-9X8N3NVR1B');`}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
